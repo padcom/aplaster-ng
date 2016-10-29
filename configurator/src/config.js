d951c682372bc72adc7ea77d93d49f1d830d29c8
Matthias Hryniszak <padcom@gmail.com>
2016-10-29 23:15:59 +0200

    Added Digital input/output difinition

--- configurator/src/config.js
+++ configurator/src/config.js
@@ -50,6 +50,26 @@ const RS232 = {
   }
 }
 
+const Digital = {
+  props: {
+    name: {
+      title: 'Digital input/output',
+      type: 'string'
+    },
+    index: {
+      title: 'Index',
+      type: 'number',
+      readonly: true
+    },
+    direction: {
+      title: 'Direction',
+      type: 'number',
+      min: 0,
+      max: 0
+    }
+  }
+}
+
 export const config = {
   modules: [ {
     type: Module,
@@ -63,6 +83,46 @@ export const config = {
       bits: 8,
       parity: 0,
       stop: 1
+    }, {
+      type: Digital,
+      name: 'Output 1',
+      index: 0,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 2',
+      index: 1,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 3',
+      index: 2,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 4',
+      index: 3,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 5',
+      index: 4,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 6',
+      index: 5,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 7',
+      index: 6,
+      direction: 0
+    }, {
+      type: Digital,
+      name: 'Output 8',
+      index: 7,
+      direction: 0
     } ]
   } ]
 }
