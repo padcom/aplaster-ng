12360cfa543c79affadda4e31787f3fb17a1fb92
Matthias Hryniszak <padcom@gmail.com>
2016-11-05 12:08:28 +0100

    Added folder for automatically included filters so that they don't need to be included manually

--- configurator/src/main/main.js
+++ configurator/src/main/main.js
@@ -4,9 +4,16 @@ import Vue from 'vue'
 
 import router from './router'
 
+// Include filters
+const filters = require.context('./filters', true, /\.js$/)
+filters.keys().forEach(filters)
+
 new Vue({
   router,
-
   el: '#app',
-  template: '<div><router-link to="/">Home</router-link> <router-link to="/config">Configurator</router-link> <router-view /> </div>'
+  computed: {
+    route() {
+      return this.$route
+    }
+  }
 })
